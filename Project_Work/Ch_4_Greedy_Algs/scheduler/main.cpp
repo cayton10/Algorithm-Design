09595ec4b542baede57a120ac0e8c8f0f163a853
Obi-Wan <cayton10@live.marshall.edu>
2021-02-10 13:24:07 -0500

    Program reads JSON file in schedules dir

--- Project_Work/Ch_4_Greedy_Algs/scheduler/main.cpp
+++ Project_Work/Ch_4_Greedy_Algs/scheduler/main.cpp
@@ -15,7 +15,29 @@ using namespace std;
 int main()
 {
 
+    //Store the filename as character string for reading
+    string userFile;
+    ifstream inData;
+    cout << "Enter the name of the schedule you'd like to process. IE: sched1, sched2, etc." << endl;
+    cout << "Filename: ";
+
+    getline(cin, userFile);//Read whole line from user input above
+    //Prepend string with directory
+    userFile.insert(0, "schedules/");
+    //Add file extension
+    userFile += ".txt";
+
 /* ---------------------------- READ IN JSON FILE --------------------------- */
+    
+    inData.open(userFile.c_str());
+
+    json j;
+    inData >> j;
+
+    inData.close();
+
+    cout << j["classes"] << endl;
+    
 
     return 0;
 }
